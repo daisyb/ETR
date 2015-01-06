3d721dee2e0aedee7f46bef8c977208834208d1b
camillecasino <camille.casino@gmail.com>
2015-01-06 08:35:47 -0500

    sticky and gui

--- Office.java
+++ Office.java
@@ -3,6 +3,7 @@ import java.util.*;
 import java.awt.*;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
+import java.awt.event.*;
 import java.io.File;
 import java.io.IOException;
 import java.util.logging.Level;
@@ -11,13 +12,12 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.lang.ClassLoader;
 
-public class Office extends JFrame {
+public class Office extends JFrame implements MouseListener {
     
     private JFrame frame;
     private BufferedImage image;
     private Container pane;
     private JPanel canvas;
-    
     
     public Office() {
 	setTitle("Office");
@@ -33,9 +33,31 @@ public class Office extends JFrame {
 	canvas = new Canvas();
 	canvas.update(canvas.getGraphics());
 	pane.add(canvas);
-       
+	canvas.addMouseListener(this);
     }
 
+    public void mousePressed(MouseEvent e) {
+    }
+
+    public void mouseReleased(MouseEvent e) {
+    }
+
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    public void mouseExited(MouseEvent e) {
+    }
+
+    public void mouseClicked(MouseEvent e) {
+	int x = e.getX();
+	int y = e.getY();
+	if ((x < 700 && x > 0) && (y < 700 && y > 0)) {
+	    Scramble s = new Scramble();
+	    s.setVisible(true);
+	    System.out.println("yes");
+	}
+    }
+    
     private class Canvas extends JPanel {
 	public void paintComponent(Graphics g) {
 	    super.paintComponent(g);
@@ -44,8 +66,6 @@ public class Office extends JFrame {
     }
     
     public static void main(String[] args) {
-	Scramble s = new Scramble();
-	s.setVisible(true);
 
 	Office o = new Office();
 	o.setVisible(true);
