7091f2478a6e5f87a637dc4adfcb77530bbb4211
Camille Casiño <camillecasino@Pauls-MacBook-Pro.local>
2015-01-12 08:00:15 -0500

    working on Keypad

--- Keypad.java
+++ Keypad.java
@@ -1,72 +1,75 @@
 import java.io.*;
 import java.util.*;
 import java.awt.*;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
 import java.awt.event.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
-public class Keypad extends JDialog {
+public class Keypad extends JDialog implements MouseListener{
 
-    private JPanel kpad;
+    private JPanel canvas;
+    private BufferedImage image;
     private Container pane;
-    private JButton one, two, three, four, five, six, seven, eight, nine, zero, star, pound, enter, clear;
-    private JTextArea spaces, status;
-    /*
-    private ActionListener aL = new ActionListener() {
-	    public void actionPerformed(ActionEvent ae) {
-		if (ae.getSource() == one) {
-		    
-    */
-
-
-    public Keypad() {
+    private Stuff one, two, three, four, five, six, seven, eight, nine, zero, star, pound;
+    private JTextArea instructions, spaces, status;
+   
+    public Keypad(JFrame parent, boolean modal) {
+	super(parent, true);
 	setTitle("Keypad");
-	setSize(650, 450);
+	setSize(464, 666);
 	setLocation(100, 100);
-	setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+	setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 	pane = getContentPane();
-	GroupLayout layout = new GroupLayout(pane);
-	pane.setLayout(layout);
-	spaces = new JTextArea("hello", 100, 20);
-	pane.add(spaces);
-	kpad = new JPanel();
-	one = new JButton("1");
-	two = new JButton("2");
-	three = new JButton("3");
-	four = new JButton("4");
-	five = new JButton("5");
-	six = new JButton("6");
-	seven = new JButton("7");
-	eight = new JButton("8");
-	nine = new JButton("9");
-	zero = new JButton("0");
-	star = new JButton("*");
-	pound = new JButton("#");
-	one.setLocation(180, 100);
-	one.setSize(75, 45);
-	pane.add(one);
-	two.setLocation(260, 100);
-	two.setSize(75, 45);
-	pane.add(two);
-	three.setLocation(340, 100);
-	three.setSize(75, 45);
-	pane.add(three);
-	four.setLocation(180, 150);
-	four.setSize(75, 45);
-	pane.add(four);
-	five.setLocation(260, 150);
-	five.setSize(75, 45);
-	pane.add(five); /*
-	pane.add(six);
-	pane.add(seven);
-	pane.add(eight);
-	pane.add(nine);
-	pane.add(zero);
-	*/
+	pane.setLayout(new FlowLayout());
+	try {
+	    image = ImageIO.read(new File("images/keypad.jpeg"));
+	} catch (IOException ex) {
+	    System.out.println("cannot load image");
+	}
+	canvas = new Canvas();
+	canvas.update(canvas.getGraphics());
+	pane.add(canvas);
+	canvas.addMouseListener(this);
+
+	one = new Stuff(72, 160, 205, 293);
+	two = new Stuff(191, 277, 208, 295);
+
+	setVisible(true);
     }
 
-    public static void main(String[] args) {
-	Keypad k = new Keypad();
-	k.setVisible(true);
+
+    public void mousePressed(MouseEvent e) {
     }
+    public void mouseReleased(MouseEvent e) {
+    }
+    public void mouseEntered(MouseEvent e) {
+    }
+    public void mouseExited(MouseEvent e) {
+    }
+    public void mouseClicked(MouseEvent e) {
+    }
+    
+    private class Canvas extends JPanel {
+	public void paintComponent(Graphics g) {
+	    super.paintComponent(g);
+	    g.drawImage(image, 0, 0, null);
+	}
+    }
+
+    /*-----------------------Methods-------------------*/
+	
+
+
+    //public static void main(String[] args) {
+//	Keypad k = new Keypad();
+//	k.setVisible(true);
+//  }
 
 }
