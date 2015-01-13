d2470534cdddf628a8f1e3513a2ab57202b18bd5
Daisy <dbarbanel555@gmail.com>
2015-01-12 20:11:27 -0500

    modified to extend Popup.java

--- Drawer.java
+++ Drawer.java
@@ -1,72 +1,101 @@
-import java.io.*;
-import java.util.*;
-import java.util.logging.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.awt.image.*;
-import javax.imageio.ImageIO;
 import javax.swing.*;
 
-public class Drawer extends JDialog implements MouseListener{
-    private BufferedImage image;
+//NON-RUNABLE
+//still in the works till I make the drawer images
+
+
+/*
+  creates window for looking inside either drawer
+  chages behaivior based on the boolean drawer1
+*/
+
+public class Drawer extends Popup{
     private Container pane;
     private JPanel canvas;
     private Stuff scissors, slip, phone;
-    private boolean Drawer1,hasScissors,hasPhone,hasSlip;
+    private boolean Drawer1,foundScissors,foundPhone,foundSlip;
 
-
-    public void mousePressed(MouseEvent e) {
-    }
-
-    public void mouseReleased(MouseEvent e) {
-    }
-
-    public void mouseEntered(MouseEvent e) {
-    }
-
-    public void mouseExited(MouseEvent e) {
-    }
-
+    //changes the background image based on whether the user
+    //clicked on an item or not
     public void mouseClicked(MouseEvent e) {
+	String folder = "images/drawers/";
+	int x = e.getX();
+	int y = e.getY();
 	if(Drawer1){
-	    if(scissors.withinBounds() && isActive()){
-		hasScissors = true;
+	    if(scissors.withinBounds(x,y) && isActive()){
+		foundScissors = true;
+		setImage(folder + "dempty");
+		canvas.update(canvas.getGraphics());
+		scissors.setActive(false);
+	    }
+	} else {
+	    if(phone.withinBounds(x,y) && isActive()){
+		foundPhone = true;
+		if(slip.isActive()){
+		    setImage(folder+"d2NoPhone");
+		}else{
+		    setImage(folder+"dempty");
+		}
+		canvas.update(canvas.getGraphics());
+		phone.setActive(false);
+	    }else if(slip.withinBounds(x,y) && isActive()){
+		foundlip = true;
+		if(key.isActive()){
+		    setImage(folder+"d2NoSlip");
+		}else{
+		    setImage(folder+"dempty");
+		}
+		canvas.update(canvas.getGraphics());
+		phone.setActive(false);
 	    }
 	}
-		
     }
 
+    //constructor
     public Drawer(JFrame parent, boolean modal,boolean isDrawer1){
 	super(parent, true);
 	Drawer1 = isDrawer1;
-	setDefaultOperation(Dispose_ON_CLOSE);
-	setLocation(250,150);
 	setSize(400,400);
-	setTitle("Drawer Inside");
-	pane = getContnentPane();
-	
-	try {                
-	    image = ImageIO.read(new File("painting/pBack.bmp"));
-	} catch (IOException ex) {
-	    System.out.println("oops");
+	setTitle("Inside the Drawer");
+
+	if(Drawer1){
+	    setImage("images/drawers/d1");
+	}else{
+	    setImage("images/drawers/d2");
 	}
 
+	pane = getContnentPane();
+	
 	canvas = new Canvas();
 	canvas.update(canvas.getGraphics());
 	pane.add(canvas);
 	canvas.addMouseListener(this);
 
-	
-	// scissors = new Stuff(blahlabladlhg);
-	// slip = new Stuff(badjgfhasdjkghs);
-	// phone = new Stuff(blahblahabval);
+	/*
+	  have to figure out the coordinates for these 
+	  once the image is done:
+	    scissors = new Stuff(blahlabladlhg);
+	    slip = new Stuff(badjgfhasdjkghs);
+	    phone = new Stuff(blahblahabval);
+	*/
+
 	setVisible(true);
     }
 
- 
-    private class Canvas extends JPanel {
-	public void paintComponent(Graphics g) {
-	    super.paintComponent(g);
-	    g.drawImage(image, 0, 0, null);
-	}
+    //methods to tell Room.java which items
+    //the user picked up
+    public boolean hasScissors(){
+	return foundScissors;
     }
+    
+    public boolean hasPhone(){
+	return foundPhone;
+    }
+
+    public boolean hasSlip(){
+	return foundSlip;
+    }
+    
+}
--- pBack.java
+++ pBack.java
@@ -1,84 +1,52 @@
-import java.io.*;
-import java.util.*;
-import java.util.logging.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.awt.image.*;
-import javax.imageio.ImageIO;
 import javax.swing.*;
 
-public class pBack extends JDialog implements MouseListener{
-    private BufferedImage image;
+/*
+  Creates window for picking up the key and slip items 
+  on the back of the painting
+*/
+
+public class pBack extends Popup{
     private Container pane;
     private JPanel canvas;
     private Stuff key, slip;
 
-
-    public void mousePressed(MouseEvent e) {
-    }
-
-    public void mouseReleased(MouseEvent e) {
-    }
-
-    public void mouseEntered(MouseEvent e) {
-    }
-
-    public void mouseExited(MouseEvent e) {
-    }
-
+    //changes image based on whether the user
+    //clicked on an item or not
     public void mouseClicked(MouseEvent e) {
 	int x = e.getX();
 	int y = e.getY();
-	if(key.withinBounds(x,y)&& key.isActive()){
-	    System.out.println("key");
+	if(key.withinBounds(x,y) && key.isActive()){
+	
 	    if(slip.isActive()){
-		try {                
-		    image = ImageIO.read(new File("painting/pJustSlip.bmp"));
-		} catch (IOException ex) {
-		    System.out.println("oops");
-		}
+		setImage("painting/pJustSlip.bmp");
 	    } else {
-		try {                
-		    image = ImageIO.read(new File("painting/pNothin.bmp"));
-		} catch (IOException ex) {
-		    System.out.println("oops");
-		}
+		setImage("painting/pNothin.bmp");
 	    }	
 	    canvas.update(canvas.getGraphics());
 	    key.setActive(false);
 
 	} else if (slip.withinBounds(x,y) && slip.isActive()){
-	    System.out.println("slip");
+
 	    if(key.isActive()){
-		try {                
-		    image = ImageIO.read(new File("painting/pJustKey.bmp"));
-		} catch (IOException ex) {
-		    System.out.println("oops");
-		} 
+		setImage("painting/pJustKey.bmp");
 	    }else {
-		try {                
-		    image = ImageIO.read(new File("painting/pNothin.bmp"));
-		} catch (IOException ex) {
-		    System.out.println("oops");
-		}
+		setImage("painting/pNothin.bmp");
 	    }
 	    canvas.update(canvas.getGraphics());
 	    slip.setActive(false);
 	}
     }
 
+    //constructor
     public pBack(JFrame parent, boolean modal){
 	super(parent,true);
-	setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-	setLocation(250,150);
-	setSize(400,530);
+	setSize(400,530);	
 	setTitle("Back of Poster");
+	setImage("painting/pBack.bmp");
+
 	pane = getContentPane();
-	try {                
-	    image = ImageIO.read(new File("painting/pBack.bmp"));
-	} catch (IOException ex) {
-	    System.out.println("oops");
-	}
 
 	canvas = new Canvas();
 	canvas.update(canvas.getGraphics());
@@ -87,21 +55,16 @@ public class pBack extends JDialog implements MouseListener{
 
 	key = new Stuff(100,264,116,193);
 	slip = new Stuff(86,258,268,312);
-
+	
 	setVisible(true);
-
     }
+
 
     
-    private class Canvas extends JPanel {
-	public void paintComponent(Graphics g) {
-	    super.paintComponent(g);
-	    g.drawImage(image, 0, 0, null);
-	}
-    }
-
     public static void main(String[] args){
 	pBack p = new pBack(new JFrame(),true);
 	
     }
+    
+    
 }
