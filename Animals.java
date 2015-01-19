1244092c3ae66437d3a16f0b049fe80130b2eeed
daisy <daisybarbanel@yahoo.com>
2015-01-19 11:00:43 -0500

    finished popup for stuffed animals

--- Animals.java
+++ Animals.java
@@ -6,7 +6,7 @@ import javax.swing.*;
   creates window for inside stuffed animals
   changes behaivior based on int animalnum
   if animalnum == 1 --> totoro
-  2 --> rilakuma
+  2 --> rilakkuma
   3 --> appa
 */
 
@@ -17,20 +17,88 @@ public class Animals extends Popup{
     private boolean foundKey, foundSlip;
     private int animalnum;
 
-    private void mouseClicked(MouseEvent e){
+    //changes the background image based on whether the user
+    //clicked on an item or not
+    public void mouseClicked(MouseEvent e){
 	String folder = "images/stuffed/";
 	int x = e.getX();
 	int y = e.getY();
+	if(animalnum == 1){
+	    if(slip.withinBounds(x,y) && isActive()){
+		foundSlip = true;
+		setImage(folder + "stuffedEmpty.bmp");
+		canvas.update(canvas.getGraphics());
+		slip.setActive(false);
+	    }
+	} else if (animalnum == 3){
+	    if(key.withinBounds(x,y) && isActive()){
+		foundKey = true;
+		setImage(folder + "stuffedEmpty.bmp");
+		canvas.update(canvas.getGraphics());
+		key.setActive(false);
+	    }
+	}
+
+
     }
 
+    //constructor
     public Animals(JFrame parent, boolean modal, int whichAnimal){
 	super(parent, true);
 	animalnum = whichAnimal;
-	setSize(200,400);
+	setSize(475,375);
 	if(animalnum == 1){
 	    setTitle("Inside Totoro");
-	    setImage("images/stuffed/somethinf");
+	    setImage("images/stuffed/stuffedSlip.bmp");
+	}else if (animalnum == 2){
+	    setTitle("Inside Rilakkuma");
+	    setImage("images/stuffed/stuffedEmpty.bmp");
+	}else{
+	    setTitle("Inside Appa");
+	    setImage("images/stuffed/stuffedKey.bmp");
 	}
+
+	pane = getContentPane();
+	canvas = new Canvas();
+	canvas.update(canvas.getGraphics());
+	pane.add(canvas);
+	canvas.addMouseListener(this);
+	
+	key = new Stuff(81,265,133,197);
+	slip = new Stuff(211,387,189,286);
+
+	foundSlip = false;
+	foundKey = false;
+
+    }
+
+    public Animals(JFrame parent, boolean modal){
+	this(parent,modal,2);
+    }
+
+    public void setAnimal(int whichAnimal){
+	animalnum = whichAnimal;
+	if(animalnum == 1){
+	    setTitle("Inside Totoro");
+	    setImage("images/stuffed/stuffedSlip.bmp");
+	}else if (animalnum == 2){
+	    setTitle("Inside Rilakkuma");
+	    setImage("images/stuffed/stuffedEmpty.bmp");
+	}else{
+	    setTitle("Inside Appa");
+	    setImage("images/stuffed/stuffedKey.bmp");
+	}
+	System.out.println("Dsgasd");
+    }	
+
+    //methods to tell Room.java which items
+    //the user picked up
+    public boolean hasKey(){
+	return foundKey;
+    }
+
+    public boolean hasSlip(){
+	return foundSlip;
     }
 
 }
