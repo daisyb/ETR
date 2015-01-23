f2db32c5b042df72275ff85647d89c942823d507
Daisy B <daisybarbanel@yahoo.com>
2015-01-23 08:08:28 -0500

    added driver

---
+++ Driver.java
@@ -0,0 +1,9 @@
+public class Driver{
+
+    //Creates new instance of Room.java
+    //the real meat of the project is in Room
+    public static void main(String[] args){
+	Room r = new Room();
+	r.setVisible(true);
+    }
+}
